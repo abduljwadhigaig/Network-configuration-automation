35092567fa28d551aaedb17c4dc0a249bd96bb97
Abduljwad Higaig <77650507+abduljwadhigaig@users.noreply.github.com>
2023-04-20 12:52:19 +0200

    Add files via upload

---
+++ RESTCONF/dev_info.py
@@ -0,0 +1,53 @@
+ciscoA = {
+             "name":"ciscoA",
+             "host": "https://192.168.122.79/restconf/data/Cisco-IOS-XE-native:native",
+             "username": "cisco",
+             "password": "cisco",
+              "headers" : {"Accept": "application/yang-data+xml",
+		          "Content-Type": "application/yang-data+xml"}
+           }
+
+ciscoB = {
+            "name":"ciscoB",
+             "host": "https://192.168.122.120/restconf/data/Cisco-IOS-XE-native:native",
+             "username": "cisco",
+             "password": "cisco",
+              "headers" : {"Accept": "application/yang-data+xml",
+		          "Content-Type": "application/yang-data+xml"}
+           }
+
+ciscoC = {
+            "name":"ciscoC",
+             "host": "https://192.168.122.175/restconf/data/Cisco-IOS-XE-native:native",
+             "username": "cisco",
+             "password": "cisco",
+              "headers" : {"Accept": "application/yang-data+xml",
+		          "Content-Type": "application/yang-data+xml"}
+           }
+
+R21 = {
+             "name":"R21",
+             "host": "https://192.168.122.215/restconf/data/Cisco-IOS-XE-native:native",
+             "username": "cisco",
+             "password": "cisco",
+              "headers" : {"Accept": "application/yang-data+xml",
+		          "Content-Type": "application/yang-data+xml"}
+           }
+
+R11 = {
+            "name":"R11",
+             "host": "https://192.168.122.82/restconf/data/Cisco-IOS-XE-native:native",
+             "username": "cisco",
+             "password": "cisco",
+              "headers" : {"Accept": "application/yang-data+xml",
+		          "Content-Type": "application/yang-data+xml"}
+           }
+
+R22 = {
+            "name":"R22",
+             "host": "https://192.168.122.92/restconf/data/Cisco-IOS-XE-native:native",
+             "username": "cisco",
+             "password": "cisco",
+              "headers" : {"Accept": "application/yang-data+xml",
+		          "Content-Type": "application/yang-data+xml"}
+           }
