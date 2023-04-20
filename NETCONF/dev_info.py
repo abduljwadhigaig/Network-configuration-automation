cf0e587bb136f62ac32578ef3138e0e2dcb9d5a0
Abduljwad Higaig <77650507+abduljwadhigaig@users.noreply.github.com>
2023-04-20 12:48:25 +0200

    Add files via upload

---
+++ NETCONF/dev_info.py
@@ -0,0 +1,42 @@
+ciscoA = {
+             "host": "192.168.122.79",
+             "port": 830,
+             "username": "cisco",
+             "password": "cisco"
+
+           }
+
+ciscoB = {
+             "host": "192.168.122.120",
+             "port": 830,
+             "username": "cisco",
+             "password": "cisco"
+           }
+
+ciscoC = {
+             "host": "192.168.122.175",
+             "port": 830,
+             "username": "cisco",
+             "password": "cisco"
+           }
+
+R21 = {
+             "host": "192.168.122.215",
+             "port": 830,
+             "username": "cisco",
+             "password": "cisco"
+           }
+
+R11 = {
+             "host": "192.168.122.82",
+             "port": 830,
+             "username": "cisco",
+             "password": "cisco"
+           }
+
+R22 = {
+             "host": "192.168.122.92",
+             "port": 830,
+             "username": "cisco",
+             "password": "cisco"
+           }
